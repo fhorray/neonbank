41a47fcfcc7c040c245eb5f179cb4601e322f70b
Francy Santos <francy.horray@gmail.com>
2023-01-01 23:53:16 -0300

    slide adjustment

--- js/swiperconfig.js
+++ js/swiperconfig.js
@@ -2,7 +2,7 @@ var swiper = new Swiper(".depoimentos-slide", {
   slidesPerView: 3,
   spaceBetween: 32,
   pagination: {
-    el: "git status.swiper-pagination",
+    el: ".pagination-depo",
     clickable: true,
   },
 });
