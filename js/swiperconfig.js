a876daf9e5e027fc1b4dbc3a01a252bbbf3b624d
Francy Santos <francy.horray@gmail.com>
2023-01-03 20:00:42 -0300

    s-depoimentos reponsive

--- js/swiperconfig.js
+++ js/swiperconfig.js
@@ -5,4 +5,26 @@ var swiper = new Swiper(".depoimentos-slide", {
     el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.3,
+      spaceBetween: 16,
+    },
+    500: {
+      slidesPerView: 1.6,
+      spaceBetween: 16,
+    },
+    600: {
+      slidesPerView: 1.6,
+      spaceBetween: 16,
+    },
+    800: {
+      slidesPerView: 2.3,
+      spaceBetween: 20,
+    },
+    1150: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+  },
 });
